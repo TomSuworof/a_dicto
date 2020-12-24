88155aa2a0257bfa172031a2bedc7d9109d7e36d
TomSuworof <rc1995king@gmail.com>
2020-12-24 15:15:08 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/a_dicto/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.a_dicto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primarySwatch: Colors.orange,
+    ),
+    home: MyHomePage(),
+  );
+}
+
+// import 'package:flutter/material.dart';
+// import 'package:audio_recorder/audio_recorder.dart';
+//
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'A-Dicto',
+//       theme: ThemeData(
+//         primarySwatch: Colors.red,
+//         visualDensity: VisualDensity.adaptivePlatformDensity,
+//       ),
+//       home: MyHomePage(title: 'A-Dicto'),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key key, this.title}) : super(key: key);
+//
+//   final String title;
+//
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
+//   IconData icon = Icons.mic;
+//   bool isRecording = true; // await AudioRecorder.isRecording;
+//
+//   // bool hasPermissions;
+//   //
+//   // Future<bool> _preparingForRecord() async {
+//   //   hasPermissions = await AudioRecorder.hasPermissions;
+//   //   return hasPermissions;
+//   // }
+//
+//
+//   void _startRecord() {
+//     setState(() {
+//       bool hasPermissions = true;
+//       if (hasPermissions) {
+//         if (isRecording) {
+//           icon = Icons.stop;
+//           isRecording = false;
+//         } else {
+//           icon = Icons.mic;
+//           isRecording = true;
+//         }
+//       }
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//       //   child: Column(
+//       //     mainAxisAlignment: MainAxisAlignment.center,
+//       //     children: <Widget>[
+//       //       Text(
+//       //         'You have pushed the button this many times:',
+//       //       ),
+//       //       Text(
+//       //         '$_counter',
+//       //         style: Theme.of(context).textTheme.headline4,
+//       //       ),
+//       //     ],
+//       //   ),
+//       // ),
+//       child: L
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _startRecord,
+//         child: Icon(icon),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
